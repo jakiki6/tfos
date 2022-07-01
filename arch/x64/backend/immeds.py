5f3951e2e8942c186282851c3ea40434a8b48e19
Jakob Kirsch <jakob.kirsch@web.de>
2022-07-01 16:09:52 +0200

    x64: add IF and THEN

--- arch/x64/backend/immeds.py
+++ arch/x64/backend/immeds.py
@@ -21,6 +21,21 @@ def end_word(buf, binary, imm, dict, back):
     binary.seek(p)
 imms[";"] = end_word
 
+def cond_clause(buf, binary, imm, dict, back):
+    binary.write(b("4883ED0848837D00000F84"))
+    stack.append(binary.tell())
+    binary.write(b("00000000"))
+imms["IF"] = cond_clause
+
+def then_clause(buf, binary, imm, dict, back):
+    target = stack.pop()
+    rel = binary.tell() - target - 4
+    p = binary.tell()
+    binary.seek(target)
+    binary.write(rel.to_bytes(4, "little", signed=True));
+    binary.seek(p)
+imms["THEN"] = then_clause
+
 def comment(buf, binary, imm, dict, back):
     while word(buf) != ")":
         continue
