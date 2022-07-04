32101329b46f9059c4ec3fe97b886b298a9e4e43
Jakob Kirsch <jakob.kirsch@web.de>
2022-07-04 22:29:07 +0200

    x64: more useless stuff

--- arch/x64/init/main.fs
+++ arch/x64/init/main.fs
@@ -11,6 +11,10 @@ until ;
 LIT" booting tfos..." print
 
 $f0000 $b8140 480 mem-copy
+val ptr
+$ffff5 to ptr
+$b8078 to fb
+8 begin ptr c@ fb c! ptr 1+ to ptr fb 2 + to fb 1- dup 0 = until drop
 
 begin
   cookie-push
