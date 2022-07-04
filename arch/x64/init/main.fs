862d797dff4ccdb85cf77685f77fda0e9cb1e77d
Jakob Kirsch <jakob.kirsch@web.de>
2022-07-04 20:53:08 +0200

    x64: add new words, fix not and add some test

--- arch/x64/init/main.fs
+++ arch/x64/init/main.fs
@@ -2,18 +2,18 @@ val fb
 $b8000 to fb
 
 : print begin
-  dup c@ fb c! fb 1+ $1f c! fb 2 + to fb 1+ dup c@ 0 =
+  dup c@ fb c! fb 1+ $1f c! fb 2 + to fb 1+ dup c@ not
 until ;
 
-LIT" tfos" print
+LIT" booting tfos..." print
 
-val color
-begin $b8140
-  begin dup color swap c! 1 + dup $b8fa0 = until
-drop color
-  if 0 to color
-  else rng to color
-  then
+$f0000 $b8140 480 mem-copy
+
+begin
+  $100
+  begin $b8320
+    begin dup rng swap c! 1+ dup $b8fa0 = until
+  drop 1- dup not until
+
+  $b8090 @ 1+ $b8090 !
 again
-
-arch-hlt
