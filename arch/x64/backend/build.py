f811aebe6276478be28fb7413c079ad51ae80ea1
Jakob Kirsch <jakob.kirsch@web.de>
2022-07-05 21:25:52 +0200

    x64: add mul, div and mod

--- arch/x64/backend/build.py
+++ arch/x64/backend/build.py
@@ -4,6 +4,11 @@ strings = {}
 
 for root, _, files in os.walk(os.path.join(os.path.dirname(__file__), "asm")):
     for _fn in files:
+        if _fn.startswith("$") and len(_fn) > 1:
+            name = bytes.fromhex(_fn[1:]).decode()
+        else:
+            name = _fn
+
         fn = os.path.join(root, _fn)
 
         cmd = ["nasm",  "-f", "bin", "-o", "/tmp/build.bin", fn]
@@ -11,7 +16,7 @@ for root, _, files in os.walk(os.path.join(os.path.dirname(__file__), "asm")):
         subprocess.run(cmd, check=True)
 
         with open("/tmp/build.bin", "rb") as file:
-            strings[_fn] = file.read()
+            strings[name] = file.read()
 
 with open(os.path.join(os.path.dirname(__file__), "strings.txt"), "w") as file:
     for k, v in strings.items():
