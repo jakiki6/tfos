d8b0e683a681101bd72c7163358aef5f7df85815
Jakob Kirsch <jakob.kirsch@web.de>
2022-08-04 00:41:55 +0200

    x64: fix paging (0b11 is definitely equal to 2)

--- arch/x64/cpu/paging.fs
+++ arch/x64/cpu/paging.fs
@@ -11,8 +11,8 @@ val cpu-paging-pml2
   cpu-paging-pml3 $1000 mem-wipe
   cpu-paging-pml2 $1000 mem-wipe
 
-  cpu-paging-pml3 $02 or cpu-paging-pml4 !
-  cpu-paging-pml2 $02 or cpu-paging-pml3 !
+  cpu-paging-pml3 $03 or cpu-paging-pml4 !
+  cpu-paging-pml2 $03 or cpu-paging-pml3 !
 
   512 do
     i 21 << $83 or cpu-paging-pml2 i 3 << + !
