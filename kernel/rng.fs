14a91ea5d4c04ba3721eb8d30b802ef69b749e45
Jakob Kirsch <jakob.kirsch@web.de>
2022-08-04 01:25:12 +0200

    rng: change from lcg to xorshift64

--- kernel/rng.fs
+++ kernel/rng.fs
@@ -5,6 +5,10 @@ val rng-seed
 ;
 
 : rng
-  rng-seed $5deece66d * $b + dup to rng-seed
-  16 >>
+  rng-seed
+    dup 12 >> xor
+    dup 25 << xor
+    dup 27 >> xor
+    $2545f4914f6cdd1d *
+  dup to rng-seed
 ;
