b7c5b71ea6e4bf363c3441232320db958885171b
Jakob Kirsch <jakob.kirsch@web.de>
2022-08-27 02:37:21 +0200

    sched: refine design

--- kernel/sched.fs
+++ kernel/sched.fs
@@ -1,6 +1,4 @@
 val sched-head
-val sched-head-main
-val sched-head-idle
 
 (
   format of a sched entry:
@@ -8,30 +6,24 @@ val sched-head-idle
     u64 next
     u64 waitfor [ 0 means no wait ]
     u64 waitval
+    u64 prev
 )
 
 : sched-init
-  32 mem-alloc dup
+  40 mem-alloc
     to sched-head
-    to sched-head-main
-  0 sched-head-main !
-  sched-head-main sched-head-main 8 + !
-  0 sched-head-main 16 + !
+  0 sched-head !
+  sched-head sched-head 8 + !
+  sched-head sched-head 32 + !
+  0 sched-head 16 + !
 
-  ' sched-idle cpu-sched-add
-  24 mem-alloc 
-    to sched-head-idle
-  sched-head-idle !
-  sched-head-main sched-head-idle 8 + !
-  0 sched-head-idle 16 + !
+  ' sched-idle sched-add
 ;
 
-val _sched-head
 : sched-handler
   sched-head !
 
   sched-head 8 + @ to sched-head
-  sched-head to _sched-head
   begin
     sched-head 16 + @
       dup 0 = if
@@ -43,23 +35,35 @@ val _sched-head
       then
 
     sched-head 8 + @ to sched-head
-    dup _sched-head = drop 1
-  until
-
-  sched-head-idle to sched-head
-  sched-head @
+  again
 ;
 
 : sched-add ( addr -- )
   cpu-irq-disable
   cpu-sched-add
   sched-head 8 + @ swap
-  32 mem-alloc
+  40 mem-alloc
     dup 16 + 0 swap !
     dup sched-head 8 + !
     dup rot> !
+    dup dup 32 + !
     8 + !
   cpu-irq-enable
+;
+
+: sched-exit
+  cpu-irq-disable
+
+  ( update the next field of the previous task )
+  sched-head 8 + @ sched-head 32 + @ 8 + !
+
+  ( update the prev field of the next task )
+  sched-head 32 + @ sched-head 8 + @ 32 + !
+
+  cpu-irq-enable
+
+  ( we will be killed after the next timer irq so we just busy loop )
+  begin arch-pause again
 ;
 
 : sched-waitfor
@@ -72,4 +76,4 @@ val _sched-head
   cpu-sched-wait
 ;
 
-: sched-idle begin arch-pause again ;
+: sched-idle begin again ;
