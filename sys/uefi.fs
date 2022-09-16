3c6bb5cd87318dd90446b86a6e77f0e68d20eea3
Jakob Kirsch <jakob.kirsch@web.de>
2022-09-16 22:11:27 +0200

    uefi: add memory map

--- sys/uefi.fs
+++ sys/uefi.fs
@@ -18,22 +18,19 @@ val uefi-memmap-desc-ver
   uefi-systable s-uefi-systable/boot-services to uefi-srv-boot
   uefi-systable s-uefi-systable/runtime-services to uefi-srv-runtime
 
-  ( get memory map, make bogus call to get memory map size )
-  v' uefi-memmap-size uefi-memmap-ptr v' uefi-memmap-key v' uefi-memmap-desc-size v' uefi-memmap-desc-ver
-    uefi-srv-boot s-uefi-srv-boot/get-memory-map uefi-call $ff and 2 != if
-      LIT" uefi: cannot determine memory map size" panic
-    then
-
-  ( make space for 2 segmentations )
-  uefi-memmap-size uefi-memmap-desc-size 2 * + to uefi-memmap-size
-
-  ( allocate space )
+  ( get memory map, allocate space )
+  $10000 to uefi-memmap-size
   2 uefi-memmap-size v' uefi-memmap-ptr 0 0 0
     uefi-srv-boot s-uefi-srv-boot/allocate-pool uefi-call if
       LIT" uefi: cannot allocate buffer for memory map" panic
     then
 
-  LIT" [*] uefi: memory map is at " klog uefi-memmap-ptr klog-buf utils-printh klog-nl
+  LIT" [*] uefi: memory map is at " klog uefi-memmap-ptr klog-buf utils-printh LIT"  size " klog uefi-memmap-size klog-buf utils-printh klog-nl
+
+  v' uefi-memmap-size uefi-memmap-ptr v' uefi-memmap-key v' uefi-memmap-desc-size v' uefi-memmap-desc-ver 0
+    uefi-srv-boot s-uefi-srv-boot/get-memory-map uefi-call if
+      LIT" uefi: cannot obtain memory map" panic
+    then
 
   begin again
 ;
